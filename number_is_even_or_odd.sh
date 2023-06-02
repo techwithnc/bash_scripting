a878522b356beb7189a4a00f053d0c51ba24f5b1
TechWithNC <techwithnc.techwithnc@gmail.com>
2023-06-02 22:01:14 +0630

    add commends for author_info

--- number_is_even_or_odd.sh
+++ number_is_even_or_odd.sh
@@ -1,15 +1,18 @@
-###################################################
-###     TechWithNC                              ###
-###     https://github.com/techwithnc           ###
-###     https://www.youtube.com/@techwithnc     ###
-###################################################
-
 #!/bin/bash
+
+#####################################################################################
+### 		    Author:		TechWithNC (www.youtube.com/@techwithnc)              ###
+### 		    Date Created:	29/May/2023                                       ###
+### 		    Last Modified:  2/Jun/2023                                        ###
+### 		    Decription:	This script will check number is even or odd          ###
+###	 	        Usges:		./number_is_even_or_odd.sh                            ###
+#####################################################################################
+
 echo "Welcome! This script will check number is even or odd."
 read -p "What is your name? : " NAME
 USER_INPUT=y
 while [[ $USER_INPUT = y ]]; do
-read -p "Enter number : " VAR
+read -rp "Enter number : " VAR
    if [[ $VAR =~ ^[0-9]+$ ]];then
         if [ $(($VAR % 2)) -eq 0 ]; then 
             echo "Hello! $NAME , The number you enter is Even!"
@@ -19,6 +22,6 @@ read -p "Enter number : " VAR
    else
       echo "Please enter only numerical value"
    fi
-read -p "Want to try agin? (y/n) : " USER_INPUT
+read -rp "Want to try agin? (y/n) : " USER_INPUT
 done
 echo "Thank you for using me , bye!!!"
